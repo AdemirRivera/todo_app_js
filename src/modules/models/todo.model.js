9c63c495b779d04480a015d9ee7cfaa5776c31a2
ulises.riverag <ulises.riverag@salud.gob.sv>
2023-04-17 15:05:19 -0600

    feat: creando modelo de clase y store

---
+++ src/modules/models/todo.model.js
@@ -0,0 +1,26 @@
+export class Todo_Class {
+    /**
+     * @description: este es el constructor de la clase Todo
+     * @param {String} description_param
+     */
+    constructor(description_param) {
+
+        // validación de parámetros
+        if (description_param === undefined || description_param === null) {
+            throw new Error('La descripción es obligatoria')
+            return
+        }
+
+        if (typeof description_param !== 'string') {
+            throw new Error('La descripción debe ser un string')
+            return
+        }
+
+        this.id = 1
+        this.description = description_param
+        this.done = false
+        this.created_at = new Date()
+    }
+}
+
+// develop by: Ademir Rivera 2023
